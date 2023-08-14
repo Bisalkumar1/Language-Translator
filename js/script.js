d97e049f779fd2060e394d2890ab445ded6d87d6
Bisal Kumar <kumarmodibisal@gmail.com>
2023-08-14 15:33:08 +0530

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const fromText = document.querySelector(".from-text"),
 toText = document.querySelector(".to-text"),
 exchangeIcon = document.querySelector(".exchange"),
 selectTag = document.querySelectorAll("select"),
-icons = document.querySelectorAll(".row i");
-translateBtn = document.querySelector("button"),
+icons = document.querySelectorAll(".row i"),
+translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id) =>{
     for (let country_code in countries) {
@@ -39,33 +39,47 @@ translateBtn.addEventListener("click", () => {
     translateTo = selectTag[1].value;   // getting toSelect tag value
     if(!text) return;
     toText.setAttribute("placeholder", "Translating...");
-    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;  
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;   
+    //5000-chars/day limit
     // fetching api response and returning it with parsing into js obj
     // and in another then method receiving that obj
     fetch(apiUrl).then(res => res.json()).then(data => {
         // console.log(data);
         toText.value = data.responseData.translatedText;
 
-        data.matches.forEach(data => {
-            if(data.id === 0) {
-                toText.value = data.translation;
-            }
-        });
+     
         toText.setAttribute("placeholder", "Translation");
     });
 });
 
 icons.forEach(icon => {
     icon.addEventListener("click", ({target}) => {
+        // console.log(target);   // which type of button is clicking
         if(!fromText.value || !toText.value) return;
 
-        if(target.classList.contains("fa-copy")) {
-            if(target.id == "from") {
+        if(target.classList.contains("fa-copy")) {  //if clicked icon has copy button 
+            // if clicked icon has from id, copy the fromTextarea value else copy the ToTextarea value
+            if(target.id == "from") {   
                 navigator.clipboard.writeText(fromText.value); // copy from from text
             } else {
                 navigator.clipboard.writeText(toText.value);  // copy from totext
             }
              }
+             else{
+                let utterance;
+ // if clicked icon has from id, speak the fromTextarea value else speak the ToTextarea value
+
+                if(target.id=="from"){
+ utterance = new SpeechSynthesisUtterance(fromText.value);
+ utterance.lang=selectTag[0].value;          //setting utterance language to fromSelect tag value
+                }
+                else{
+                    utterance=new SpeechSynthesisUtterance(toText.value);
+                    utterance.lang=selectTag[1].value;   //setting utterance language to toSelect tag value
+                }
+                speechSynthesis.speak(utterance); // speak the passed utterance
+             }
         
     });
+
 });
